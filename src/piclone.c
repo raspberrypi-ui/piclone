c8d05eb9c557da1178e29494c875aa59b0f3b097
spl <simon@raspberrypi.org>
2016-02-11 18:24:47 +0000

    Selection of source device added.

--- src/piclone.c
+++ src/piclone.c
@@ -19,11 +19,15 @@
 /* Global variables for window values */
 
 /* Controls */
-static GtkWidget *main_dlg, *msg_dlg, *status, *progress, *cancel, *dev_cb, *start_btn;
+static GtkWidget *main_dlg, *msg_dlg, *status, *progress, *cancel, *to_cb, *from_cb, *start_btn;
+
+/* device names */
+char src_dev[32], dst_dev[32];
 
 /* mount points */
 char src_mnt[32], dst_mnt[32];
 
+/* flag to show that copy thread is running */
 char copying;
 
 /* partition data */
@@ -41,6 +45,8 @@ typedef struct
 } partition_t;
 
 partition_t parts[MAXPART];
+
+
 
 static void get_string (char *cmd, char *name)
 {
@@ -63,12 +69,20 @@ static gboolean close_msg (gpointer data)
 
 static gpointer copy_thread (gpointer data)
 {
-	char buffer[256];
-	sprintf (buffer, "sudo cp -ax %s/. %s/.", src_mnt, dst_mnt);
-	system (buffer);
+	char ctbuffer[256];
+	sprintf (ctbuffer, "sudo cp -ax %s/. %s/.", src_mnt, dst_mnt);
+	system (ctbuffer);
 	copying = 0;
 	return NULL;
 }
+
+static void backup_thread_msg (char *msg)
+{
+    gtk_widget_set_visible (GTK_WIDGET (progress), FALSE);
+    gtk_label_set_text (GTK_LABEL (status), msg);
+    gtk_button_set_label (GTK_BUTTON (cancel), _("OK"));
+}
+
 
 static gpointer backup_thread (gpointer data)
 {
@@ -78,17 +92,39 @@ static gpointer backup_thread (gpointer data)
     double prog;
     FILE *fp;
 
-    gtk_label_set_text (GTK_LABEL (status), _("Preparing card..."));
+    // check the source has an msdos partition table, or all else is for naught...
+    sprintf (buffer, "sudo parted %s unit s print | tail -n +4 | head -n 1", src_dev);  
+    fp = popen (buffer, "r");
+    if (fp == NULL)
+    {
+        backup_thread_msg (_("Unable to read source"));
+        return;
+    }
+    if (fgets (buffer, sizeof (buffer) - 1, fp) != NULL)
+    {
+        if (strncmp (buffer, "Partition Table: msdos", 22))
+        {
+            backup_thread_msg (_("Non-MSDOS partition table on source"));
+            return;
+        }
+    }
+    else
+    {
+        backup_thread_msg (_("Unable to read source"));
+        return;
+    }
+
+    gtk_label_set_text (GTK_LABEL (status), _("Preparing target..."));
 
     // unmount any partitions on the target device
     for (n = 9; n >= 1; n--)
     {
-        sprintf (buffer, "sudo umount %s%d", (char *) data, n);
+        sprintf (buffer, "sudo umount %s%d", dst_dev, n);
         system (buffer);
     }
 
     // wipe the FAT on the target
-    sprintf (buffer, "sudo dd if=/dev/zero of=%s bs=512 count=1", (char *) data);
+    sprintf (buffer, "sudo dd if=/dev/zero of=%s bs=512 count=1", dst_dev);
     system (buffer);
     
     // prepare temp mount points
@@ -97,7 +133,7 @@ static gpointer backup_thread (gpointer data)
     //printf ("srcmnt = %s dstmnt = %s\n", src_mnt, dst_mnt);
     
     // prepare the new FAT
-    sprintf (buffer, "sudo parted %s mklabel msdos", (char *) data);
+    sprintf (buffer, "sudo parted %s mklabel msdos", dst_dev);
     system (buffer);
     
     // read in the source partition table
@@ -105,7 +141,7 @@ static gpointer backup_thread (gpointer data)
     n = 0;
     while (1)
     {
-        sprintf (buffer, "sudo parted /dev/mmcblk0 unit s print | tail -n +%d | head -n 1", n + 8);  
+        sprintf (buffer, "sudo parted %s unit s print | tail -n +%d | head -n 1", src_dev, n + 8);
         fp = popen (buffer, "r");
         if (fp == NULL) break;
         if (fgets (buffer, sizeof (buffer) - 1, fp) != NULL)
@@ -129,14 +165,14 @@ static gpointer backup_thread (gpointer data)
 	    // create the partition
 	    if (!strcmp (parts[p].ptype, "extended"))
 	    {
-		    sprintf (buffer, "sudo parted -s %s -- mkpart extended %lds -1s", (char *) data, parts[p].start);
+		    sprintf (buffer, "sudo parted -s %s -- mkpart extended %lds -1s", dst_dev, parts[p].start);
 		}
 	    else
 	    {
 		    if (p == (n - 1))
-			    sprintf (buffer, "sudo parted -s %s -- mkpart %s %s %lds -1s", (char *) data, parts[p].ptype, parts[p].ftype, parts[p].start);
+			    sprintf (buffer, "sudo parted -s %s -- mkpart %s %s %lds -1s", dst_dev, parts[p].ptype, parts[p].ftype, parts[p].start);
 		    else
-			    sprintf (buffer, "sudo parted -s %s mkpart %s %s %lds %lds", (char *) data, parts[p].ptype, parts[p].ftype, parts[p].start, parts[p].end);
+			    sprintf (buffer, "sudo parted -s %s mkpart %s %s %lds %lds", dst_dev, parts[p].ptype, parts[p].ftype, parts[p].start, parts[p].end);
 		}
 		system (buffer);
 		
@@ -146,20 +182,20 @@ static gpointer backup_thread (gpointer data)
 		// create file systems
         if (!strncmp (parts[p].ftype, "fat", 3))
         {
-            sprintf (buffer, "sudo mkfs.fat %s%d", (char *) data, parts[p].pnum);
+            sprintf (buffer, "sudo mkfs.fat %s%d", dst_dev, parts[p].pnum);
             system (buffer);
         } 
         if (!strcmp (parts[p].ftype, "ext4"))
         {
-            sprintf (buffer, "sudo mkfs.ext4 -F %s%d", (char *) data, parts[p].pnum);
+            sprintf (buffer, "sudo mkfs.ext4 -F %s%d", dst_dev, parts[p].pnum);
             system (buffer);
         } 
 
         // set the flags        
         if (!strcmp (parts[p].flags, "lba"))
-            sprintf (buffer, "sudo parted %s set %d lba on", (char *) data, parts[p].pnum);
+            sprintf (buffer, "sudo parted %s set %d lba on", dst_dev, parts[p].pnum);
         else
-            sprintf (buffer, "sudo parted %s set %d lba off", (char *) data, parts[p].pnum);
+            sprintf (buffer, "sudo parted %s set %d lba off", dst_dev, parts[p].pnum);
         system (buffer);
         
         prog = p + 1;
@@ -175,12 +211,15 @@ static gpointer backup_thread (gpointer data)
 		gtk_label_set_text (GTK_LABEL (status), buffer);
 	
 		// mount partitions
-		sprintf (buffer, "sudo mount %s%d %s", (char *) data, parts[p].pnum, dst_mnt);
+		sprintf (buffer, "sudo mount %s%d %s", dst_dev, parts[p].pnum, dst_mnt);
 		system (buffer);
-		sprintf (buffer, "sudo mount /dev/mmcblk0p%d %s", parts[p].pnum, src_mnt);
+		if (!strcmp (src_dev, "/dev/mmcblk0"))
+		    sprintf (buffer, "sudo mount %sp%d %s", src_dev, parts[p].pnum, src_mnt);
+		else
+		    sprintf (buffer, "sudo mount %s%d %s", src_dev, parts[p].pnum, src_mnt);
 		system (buffer);
  		
-		//sudo cp -axv $src/. $dst/.
+		// start the copy itself in a new thread
 		copying = 1;
 		g_thread_new (NULL, copy_thread, NULL);	
 		
@@ -188,17 +227,17 @@ static gpointer backup_thread (gpointer data)
 		sprintf (buffer, "sudo du -s %s", src_mnt);
 		get_string (buffer, res);
 		sscanf (res, "%ld", &srcsz);
-		while (copying) //!!! check the cancel button in here....
+		sprintf (buffer, "sudo du -s %s", dst_mnt);
+		while (copying) //!!! check the cancel button in here somehow?
 		{
-			sprintf (buffer, "sudo du -s %s", dst_mnt);
 			get_string (buffer, res);
 			sscanf (res, "%ld", &dstsz);
 			prog = dstsz;
 			prog /= srcsz;
 			gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR (progress), prog);
-			printf ("%ld %ld %f\n", dstsz, srcsz, prog);
-			if (dstsz >= srcsz) break;
-			sleep (5);
+			//printf ("%ld %ld %f\n", dstsz, srcsz, prog);
+			//if (dstsz >= srcsz) break;
+			sleep (10);
 		}
 			
 		// unmount partitions
@@ -206,18 +245,30 @@ static gpointer backup_thread (gpointer data)
 		system (buffer);
 		sprintf (buffer, "sudo umount %s", src_mnt);
 		system (buffer);
-   }
-    g_idle_add (close_msg, NULL);
+    }
+
+    backup_thread_msg (_("Copy complete"));
     return NULL;
 }
 
-static void on_cancel ()
+static void on_cancel (void)
 {
     g_idle_add (close_msg, NULL);
 }
 
-static void on_start ()
+static void on_start (void)
 {
+    // set up source and target devices
+    strcpy (dst_dev, gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT (to_cb)));
+
+    if (!strcmp (_("Internal SD card"), gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT (from_cb))))
+        sprintf (src_dev, "/dev/mmcblk0");
+    else
+        strcpy (src_dev, gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT (from_cb)));  
+
+    // don't do anything if src == dest
+    if (!strcmp (src_dev, dst_dev)) return;
+
     // show the progress dialog
     msg_dlg = (GtkWidget *) gtk_dialog_new ();
     gtk_window_set_title (GTK_WINDOW (msg_dlg), "");
@@ -233,7 +284,7 @@ static void on_start ()
     gtk_container_set_border_width (GTK_CONTAINER (box), 10);
     gtk_container_add (GTK_CONTAINER (frame), box);
         
-    status = (GtkWidget *) gtk_label_new (_("Copying partition 1..."));
+    status = (GtkWidget *) gtk_label_new (_("Checking source..."));
     gtk_box_pack_start (GTK_BOX (box), status, FALSE, FALSE, 5);
     progress = (GtkWidget *) gtk_progress_bar_new ();
     gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR (progress), 0.0);
@@ -245,7 +296,7 @@ static void on_start ()
     gtk_widget_show_all (GTK_WIDGET (msg_dlg));
 
     // launch a thread with the system call to run the backup
-    g_thread_new (NULL, backup_thread, gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT (dev_cb)));
+    g_thread_new (NULL, backup_thread, NULL);
 }
 
 
@@ -276,18 +327,32 @@ int main (int argc, char *argv[])
 	gtk_builder_add_from_file (builder, PACKAGE_DATA_DIR "/piclone.ui", NULL);
 	main_dlg = (GtkWidget *) gtk_builder_get_object (builder, "dialog1");
 
-	//dev_cb = (GtkWidget *) gtk_builder_get_object (builder, "comboboxtext1");
-
-
-
 	GtkWidget *table = (GtkWidget *) gtk_builder_get_object (builder, "table1");
-	dev_cb = (GtkWidget *)  (GObject *) gtk_combo_box_text_new ();
-	gtk_table_attach (GTK_TABLE (table), GTK_WIDGET (dev_cb), 1, 2, 1, 2, GTK_FILL | GTK_SHRINK, GTK_FILL | GTK_SHRINK, 0, 0);
-	gtk_widget_show_all (GTK_WIDGET (dev_cb));
+	to_cb = (GtkWidget *)  (GObject *) gtk_combo_box_text_new ();
+	gtk_table_attach (GTK_TABLE (table), GTK_WIDGET (to_cb), 1, 2, 1, 2, GTK_FILL | GTK_SHRINK, GTK_FILL | GTK_SHRINK, 0, 0);
+	gtk_widget_show_all (GTK_WIDGET (to_cb));
 
+	if (dip = opendir ("/sys/block"))
+	{
+	    while (dit = readdir (dip))
+        {
+            if (!strncmp (dit->d_name, "sd", 2))
+            {
+                // might want to do something with g_drive_get_name here at some point...
+                sprintf (buffer, "/dev/%s",  dit->d_name);
+                gtk_combo_box_append_text (GTK_COMBO_BOX (to_cb), buffer);
+                found = 1;
+            }
+        }
+	    closedir (dip);
+	}
+	if (found) gtk_combo_box_set_active (GTK_COMBO_BOX (to_cb), 0);
 
-
-
+	from_cb = (GtkWidget *)  (GObject *) gtk_combo_box_text_new ();
+	gtk_table_attach (GTK_TABLE (table), GTK_WIDGET (from_cb), 1, 2, 0, 1, GTK_FILL | GTK_SHRINK, GTK_FILL | GTK_SHRINK, 0, 0);
+	gtk_widget_show_all (GTK_WIDGET (from_cb));
+    gtk_combo_box_append_text (GTK_COMBO_BOX (from_cb), _("Internal SD card"));
+    gtk_combo_box_set_active (GTK_COMBO_BOX (from_cb), 0);
 	
 	if (dip = opendir ("/sys/block"))
 	{
@@ -297,13 +362,12 @@ int main (int argc, char *argv[])
             {
                 // might want to do something with g_drive_get_name here at some point...
                 sprintf (buffer, "/dev/%s",  dit->d_name);
-                gtk_combo_box_append_text (GTK_COMBO_BOX (dev_cb), buffer);
+                gtk_combo_box_append_text (GTK_COMBO_BOX (from_cb), buffer);
                 found = 1;
             }
         }	
 	    closedir (dip);
 	}
-	if (found) gtk_combo_box_set_active (GTK_COMBO_BOX (dev_cb), 0);
 
 	start_btn = (GtkWidget *) gtk_builder_get_object (builder, "button1");
 	g_signal_connect (start_btn, "clicked", G_CALLBACK (on_start), NULL);
