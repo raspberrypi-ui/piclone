57c00236cf756777e37a815bfdc6661af099305a
spl <simon@raspberrypi.org>
2016-02-17 15:43:23 +0000

    Devices found by parsing parted -l rather than looking in dev.

--- src/piclone.c
+++ src/piclone.c
@@ -123,7 +123,7 @@ static int get_dev_name (char *dev, char *name)
     char buffer[256];
     FILE *fp;
 
-    sprintf (buffer, "sudo parted -l | grep -B 1 \"/dev/%s\" | head -n 1 | cut -d \":\" -f 2 | cut -d \"(\" -f 1", dev);
+    sprintf (buffer, "sudo parted -l | grep -B 1 \"%s\" | head -n 1 | cut -d \":\" -f 2 | cut -d \"(\" -f 1", dev);
     fp = popen (buffer, "r");
     if (fp == NULL || fgets (buffer, sizeof (buffer) - 1, fp) == NULL) return 0;
     buffer[strlen (buffer) - 2] = 0;
@@ -366,22 +366,8 @@ static void on_cancel (void)
 
 static void on_start (void)
 {
-    char *ptr;
-
     // close the confirm dialog
 	gtk_widget_destroy (msg_dlg);
-
-    // set up source and target devices from combobox values
-    ptr = strrchr (gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT (to_cb)), '(');
-    strcpy (dst_dev, ptr + 1);
-    dst_dev[strlen (dst_dev) - 1] = 0;
-
-    ptr = strrchr (gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT (from_cb)), '(');
-    strcpy (src_dev, ptr + 1);
-    src_dev[strlen (src_dev) - 1] = 0;
-
-    // basic sanity check - don't do anything if src == dest
-    if (!strcmp (src_dev, dst_dev)) return;
 
     // create the progress dialog
     msg_dlg = (GtkWidget *) gtk_dialog_new ();
@@ -440,6 +426,19 @@ static void on_close (void)
 static void on_confirm (void)
 {
     char buffer[256], res[256];
+    char *ptr;
+
+    // set up source and target devices from combobox values
+    ptr = strrchr (gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT (to_cb)), '(');
+    strcpy (dst_dev, ptr + 1);
+    dst_dev[strlen (dst_dev) - 1] = 0;
+
+    ptr = strrchr (gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT (from_cb)), '(');
+    strcpy (src_dev, ptr + 1);
+    src_dev[strlen (src_dev) - 1] = 0;
+
+    // basic sanity check - don't do anything if src == dest
+    if (!strcmp (src_dev, dst_dev)) return;
 
     // create the confirm dialog
     msg_dlg = (GtkWidget *) gtk_dialog_new ();
@@ -502,9 +501,8 @@ static void on_cb_changed (void)
 int main (int argc, char *argv[])
 {
 	GtkBuilder *builder;
-	DIR *dip;
-	struct dirent *dit;
-	char buffer[256];
+	char buffer[256], device[32];
+	FILE *fp;
 
 #ifdef ENABLE_NLS
     setlocale (LC_ALL, "");
@@ -544,21 +542,25 @@ int main (int argc, char *argv[])
 	// populate the comboboxes
 	gtk_combo_box_append_text (GTK_COMBO_BOX (from_cb), _("Internal SD card  (/dev/mmcblk0)"));
 	gtk_combo_box_set_active (GTK_COMBO_BOX (from_cb), 0);
-	if (dip = opendir ("/sys/block"))
-	{
-	    while (dit = readdir (dip))
-        {
-            if (!strncmp (dit->d_name, "sd", 2))
-            {
-                get_dev_name (dit->d_name, buffer);
-                sprintf (buffer, "%s  (/dev/%s)", buffer, dit->d_name);
-                gtk_combo_box_append_text (GTK_COMBO_BOX (from_cb), buffer);
-                gtk_combo_box_append_text (GTK_COMBO_BOX (to_cb), buffer);
-            }
-        }
-	    closedir (dip);
+
+    fp = popen ("sudo parted -l | grep \"^Disk /dev/\" | cut -d ' ' -f 2 | cut -d ':' -f 1", "r");
+    if (fp != NULL)
+    {
+	    while (1)
+	    {
+	        if (fgets (device, sizeof (device) - 1, fp) == NULL) break;
+
+	        if (!strncmp (device + 5, "sd", 2))
+	        {
+	            device[strlen (device) - 1] = 0;
+	            get_dev_name (device, buffer);
+	            sprintf (buffer, "%s  (%s)", buffer, device);
+	            gtk_combo_box_append_text (GTK_COMBO_BOX (from_cb), buffer);
+	            gtk_combo_box_append_text (GTK_COMBO_BOX (to_cb), buffer);
+	        }
+	    }
 	}
-	
+
 	g_object_unref (builder);
 	gtk_dialog_run (GTK_DIALOG (main_dlg));
 	gtk_widget_destroy (main_dlg);
