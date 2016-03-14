a2b1170b6eadf796cc1a6d1d23f848f07a5754ed
spl <simon@raspberrypi.org>
2016-03-14 11:08:03 +0000

    Debian build tidied.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(piclone):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="piclone" command="/usr/bin/piclone"
--- debian/piclone.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the piclone package
-#
-0 4	* * *	root	[ -x /usr/bin/piclone_maintenance ] && /usr/bin/piclone_maintenance
